90ee92b27ccd3406c9eb8c41f440910cfce15bfd
Benjamin Lapointe-Pinel <benjamin.lapointe-pinel@dfo-mpo.gc.ca>
2021-05-15 11:14:45 -0400

    configured gitignore for project

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 #include <list>
 
+#include <bitmap.hpp>
 #include <options.hpp>
 #include <pixel.hpp>
-#include <bitmap.hpp>
 
 ////////////////////////////////////////
-int main(int argc, char * argv[])
- {
+int main(int argc, char *argv[])
+{
   try
-   {
-    options opt(argc,argv);
+  {
+    options opt(argc, argv);
 
-    switch (first_of({ opt.help,
-                       opt.version,
-                       opt.compress
-                      }))
-     {
-      case 0: options::show_help(); break;
-      case 1: options::show_version(); break;
-       //case 2: compress(opt); break; // IMPLEMENT MEEE
-       //default: decompress(opt); break; // IMPLEMENT MEEEE
-     }
-   }
-
-  catch (boost::program_options::error & this_exception)
-   {
+    switch (first_of({opt.help, opt.version, opt.compress}))
+    {
+    case 0:
+      options::show_help();
+      break;
+    case 1:
+      options::show_version();
+      break;
+      // case 2: compress(opt); break; // IMPLEMENT MEEE
+      // default: decompress(opt); break; // IMPLEMENT MEEEE
+    }
+  }
+  catch (boost::program_options::error &this_exception)
+  {
     std::cerr << this_exception.what() << std::endl;
     return 1;
-   }
+  }
   return 0;
- }
+}
